c2e519099a45e77c78daef3a72feb23323d135db
orsolomon <orsolomon24@gmail.com>
2023-08-01 17:20:51 +0300

    needs to fix the hidden sales

--- public/js/feSalesBar.js
+++ public/js/feSalesBar.js
@@ -1,8 +1,5 @@
-document.getElementById('show-more-btn').addEventListener('click', function() {
-    const moreDeals = document.querySelectorAll('.sale-item:nth-child(n+6)');
-    moreDeals.forEach(deal => {
-      deal.classList.toggle('hidden-deal');
-    });
-    this.textContent = this.textContent === 'Show More' ? 'Show Less' : 'Show More';
-  });
-  
+function toggleSales() {
+  $(".hidden-deal").toggle();
+  var placeholderText = $(".hidden-deal").is(":visible") ? "Show Less" : "Show More";
+  $("#show-more-btn").text(placeholderText);
+}
