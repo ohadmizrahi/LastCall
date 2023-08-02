33097ef9fde5775812c9f2c98433ee83f2ac5ab5
orsolomon <orsolomon24@gmail.com>
2023-08-02 17:49:05 +0300

    sales css circle

--- public/js/feSalesBar.js
+++ public/js/feSalesBar.js
@@ -1,5 +1,33 @@
-function toggleSales() {
-  $(".hidden-deal").toggle();
-  var placeholderText = $(".hidden-deal").is(":visible") ? "Show Less" : "Show More";
-  $("#show-more-btn").text(placeholderText);
+function toggleSales(action) {
+  const saleItems = document.querySelectorAll('.sale-item');
+  const showLessBtn = document.getElementById('show-less-btn');
+  const showMoreBtn = document.getElementById('show-more-btn');
+
+  if (action === 'more') {
+    const hiddenDeals = document.querySelectorAll('.hidden-deal');
+
+    // Show the next 5 hidden deals
+    for (let i = 0; i < Math.min(hiddenDeals.length, 5); i++) {
+      hiddenDeals[i].classList.remove('hidden-deal');
+    }
+
+    // Update the "Show Less" button's display
+    if (document.querySelectorAll('.sale-item:not(.hidden-deal)').length > 5) {
+      showLessBtn.style.display = 'inline';
+    }
+
+    // If there are no more hidden deals, hide the "Show More" button
+    if (document.querySelectorAll('.hidden-deal').length === 0) {
+      showMoreBtn.style.display = 'none';
+    }
+  } else if (action === 'less') {
+    // Hide all but the first 5 sale items
+    for (let i = 5; i < saleItems.length; i++) {
+      saleItems[i].classList.add('hidden-deal');
+    }
+
+    // Hide the "Show Less" button and show the "Show More" button
+    showLessBtn.style.display = 'none';
+    showMoreBtn.style.display = 'inline';
+  }
 }
